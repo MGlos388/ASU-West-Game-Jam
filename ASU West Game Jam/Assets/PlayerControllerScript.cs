efaf4615d4fd1555c00b4d92668a99b991673a72
MGlos388 <mtg.shcs@gmail.com>
2024-11-02 14:28:59 -0700

    added sprint and health deduction

--- ASU West Game Jam/Assets/PlayerControllerScript.cs
+++ ASU West Game Jam/Assets/PlayerControllerScript.cs
@@ -7,17 +7,45 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float moveSpeed;
     [SerializeField] float lerpSpeed;
+    [SerializeField] float runSpeed;
+
+    [SerializeField] float maxhealth;
+    [SerializeField] float maxwater;
+    [SerializeField] float sprintHealthDeduction;
+
+    private float health;
+
     [SerializeField] Rigidbody2D rb;
+
     public Vector2 moveInput;
+    public bool running;
+
+    private void Start()
+    {
+        health = maxhealth;
+    }
 
     private void Update()
     {
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
 
+        running = Input.GetKey(KeyCode.Z);
+
+
         moveInput.Normalize();
 
-        rb.linearVelocity = Vector2.Lerp(rb.linearVelocity,moveInput * moveSpeed*Time.deltaTime,lerpSpeed);
+        Vector2 movetarget = moveInput * moveSpeed * Time.deltaTime;
+        if (running && health>0) {
+            movetarget *= runSpeed;
+            health -= sprintHealthDeduction;
+
+
+         }
+
+        rb.linearVelocity = Vector2.Lerp(rb.linearVelocity,movetarget,lerpSpeed);
+
+        Debug.Log(health);
     }
 
 
