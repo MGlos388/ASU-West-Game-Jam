fead7131576dfc730c867b07590d9d33cd450cb1
MGlos388 <mtg.shcs@gmail.com>
2024-11-03 12:43:35 -0700

    updated worm

--- ASU West Game Jam/Assets/GiantControllerScript.cs
+++ ASU West Game Jam/Assets/GiantControllerScript.cs
@@ -4,8 +4,11 @@ public class GiantControllerScript : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rb;
     [SerializeField] GameObject target;
+    [SerializeField] GameObject food;
     [SerializeField] float movespeed;
     [SerializeField] float lerpamount;
+
+    private float foodtimer = 0;
 
     private float targetAngle = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -16,6 +19,7 @@ public class GiantControllerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        foodtimer -= Time.deltaTime;    
         //rb.rotation += (Mathf.PerlinNoise(Time.time,12345))-.5f;
 
         targetAngle = 180+(180/Mathf.PI)*(Mathf.Atan2(transform.position.y-target.transform.position.y, transform.position.x - target.transform.position.x));
@@ -31,5 +35,13 @@ public class GiantControllerScript : MonoBehaviour
         transform.Rotate(new Vector3(0,0, (90.0f * (Mathf.PerlinNoise(Time.time/5, 123) - .5f))));
         transform.Translate(Vector3.right*Time.deltaTime*movespeed);
 
+
+        if (foodtimer < 0) {
+            foodtimer = 5;
+            GameObject gb = Instantiate(food);
+            gb.tag = "Material";
+            gb.GetComponent<PickUpControllerScript>().player = GameObject.Find("Player");
+            gb.transform.position = transform.position;
+        }
     }
 }
