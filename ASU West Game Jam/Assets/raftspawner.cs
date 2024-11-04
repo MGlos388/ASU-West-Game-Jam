42efb5527c26cc6903bad47173c20ddba5f34848
MGlos388 <mtg.shcs@gmail.com>
2024-11-03 18:00:09 -0700

    final

--- ASU West Game Jam/Assets/raftspawner.cs
+++ ASU West Game Jam/Assets/raftspawner.cs
@@ -11,7 +11,7 @@ public class raftspawner : MonoBehaviour
             GameObject newraft = Instantiate(raft);
             newraft.transform.position = Vector3.zero;
             float angle = (Mathf.PI*2)/raftcount;
-            angle += Random.Range(0,angle/5);
+            //angle += Random.Range(0,angle/5);
 
             newraft.transform.position += new Vector3(Mathf.Sin(angle*i)*60, Mathf.Cos(angle * i)*60, 0);
 
